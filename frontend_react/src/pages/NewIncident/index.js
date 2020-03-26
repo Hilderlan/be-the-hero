9f875feaa647ab4ea1e23a3ba578a7e9202a8ff7
Hilderlan <hilakt9698@gmail.com>
2020-03-26 16:22:58 -0300

    NewIncident page finished

---
+++ frontend_react/src/pages/NewIncident/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import './styles.css'
+
+import logoImg from '../../assets/logo.svg'
+
+export default function NewIncident(){
+  return(
+    <div className="new-incident-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be The hero"/>
+
+          <h1>Cadastrar novo caso</h1>
+          <p>Descreva aqui o caso</p>
+
+          <Link className="back-link" to="/profile">
+            <FiArrowLeft size={16} color="#E02041" />
+            Voltar para home
+          </Link>
+        </section>
+
+        <form>
+          <input placeholder="Título do caso" />
+          <textarea placeholder="Descrição" />
+          <input placeholder="Valor em reais" />
+
+          <button className="button" type="submit">Cadastrar</button>
+        </form>
+      </div>
+    </div>
+  );
+}
